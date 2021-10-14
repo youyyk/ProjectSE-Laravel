9634ddcffeea4b22a8f596ec4229dee3752908b0
iammlina <yosita.jin@ku.th>
2021-10-14 23:28:46 +0700

    merge branch youyyk into lina, add chart view but work just count

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -22,7 +22,7 @@ class BillFactory extends Factory
     public function definition()
     {
         return [
-            'total' => $this->faker->numberBetween(100,500),
+            'total' => $this->faker->numberBetween(100,1000),
             'status' => $this->faker->boolean(),
         ];
     }
